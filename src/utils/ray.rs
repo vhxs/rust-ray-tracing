66e161a587ccb00d292f2b0e45fbcd75935404af
Vikram Saraph <vikram.saraph.22@gmail.com>
2024-11-09 15:06:22 -0500

    delete some code

--- src/utils/ray.rs
+++ src/utils/ray.rs
@@ -18,46 +18,6 @@ impl Ray {
     }
 }
 
-// pub fn ray_color(ray: &Ray) -> Color {
-//     let t = hit_sphere(
-//         &Point3 {
-//             x: 0.,
-//             y: 0.,
-//             z: -1.,
-//         },
-//         &0.5,
-//         &ray,
-//     );
-
-//     if t > 0. {
-//         let vector = ray.at(t)
-//             - Vec3 {
-//                 x: 0.,
-//                 y: 0.,
-//                 z: -1.,
-//             };
-//         let normal = unit_vector(&vector);
-//         return Color {
-//             x: normal.x + 1.,
-//             y: normal.y + 1.,
-//             z: normal.z + 1.,
-//         } * 0.5;
-//     }
-
-//     let unit_direction = unit_vector(&ray.direction);
-//     let a = unit_direction.y * 0.5 + 1.;
-//     return Color {
-//         x: 1.,
-//         y: 1.,
-//         z: 1.,
-//     } * (1. - a)
-//         + Color {
-//             x: 0.5,
-//             y: 0.7,
-//             z: 1.0,
-//         } * a;
-// }
-
 pub fn ray_color(ray: &Ray, world: &dyn Hittable) -> Color {
     let mut hit_record = HitRecord::default();
     if world.hit(ray, &0., &f64::INFINITY, &mut hit_record) {
--- src/utils/sphere.rs
+++ src/utils/sphere.rs
@@ -3,20 +3,6 @@ use super::point3::Point3;
 use super::ray::Ray;
 use super::vec3::dot;
 
-pub fn hit_sphere(center: &Point3, radius: &f64, ray: &Ray) -> f64 {
-    let oc = *center - ray.origin;
-    let a = dot(&ray.direction, &ray.direction);
-    let b = -2. * dot(&ray.direction, &oc);
-    let c = dot(&oc, &oc) - radius * radius;
-    let discrminant = b * b - 4. * a * c;
-
-    if discrminant < 0. {
-        return -1.;
-    } else {
-        return (-b - discrminant.sqrt()) / (2. * a);
-    }
-}
-
 #[derive(Default)]
 pub struct Sphere {
     pub center: Point3,
